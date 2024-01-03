973b547a25e2bb9a57fd6690152746968895823a
Arthur Dezingrini <Arthurdezingrini15@gmail.com>
2024-01-03 15:51:56 -0300

    jogada especial en passant

--- Xadrez/Xadrez/PartidaDeXadrez.cs
+++ Xadrez/Xadrez/PartidaDeXadrez.cs
@@ -140,6 +140,20 @@ namespace Xadrez
                 throw new TabuleiroException("Voce nao pode se colocar em xeque!");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -158,8 +172,6 @@ namespace Xadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
             {
